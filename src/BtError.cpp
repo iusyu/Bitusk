778cfe2105e150e468d4882567c9975ab8eb9a93
IYNUS <iuamyy@163.com>
2021-05-06 00:22:43 +0800

    fix undefined ref Error
    static inline function stranger error

--- src/BtError.cpp
+++ src/BtError.cpp
@@ -10,88 +10,88 @@
  */
 
 
-#include "include/BtError.h"
+#include "BtError.h"
+#include <string>
 
-
-inline
+ 
 const std::string ErrorCatcher::fileFdError() {
 	return "FILE_FD_ERR";
 }
 
 
-inline
+ 
 const std::string ErrorCatcher::fileReadError() {
 	return "FILE_READ_ERR";
 }
 
 
-inline
+ 
 const std::string ErrorCatcher::fileWriteError() {
 	return "INVALID_BTFILE_ERR";
 }
 
 
-inline
+ 
 const std::string ErrorCatcher::invalidSocketError() {
 	return "INVALID_SOCKET_ERR";
 }
 
 
-inline
+ 
 const std::string ErrorCatcher::invalidTrackerUrlError() {
 	return "INVALID_TRACKER_URL_ERR";
 }
 
 
-inline
+ 
 const std::string ErrorCatcher::invalidTrackerReplyError() {
 	return "INVALID_TRACKER_REPLY_ERR";
 }
 
 
-inline
+ 
 const std::string ErrorCatcher::invalidHashError() {
 	return "INVALID_HASH_ERR";
 }
 
 
-inline
+ 
 const std::string ErrorCatcher::invalidMessageError() {
 	return "INVALID_MESSAGE_ERR";
 }
 
 
-inline
+ 
 const std::string ErrorCatcher::invalidParameterError() {
 	return "INVALID_PARAMETER_ERR";
 }
 
 
-inline
+ 
 const std::string ErrorCatcher::failedAllocateMemError() {
 	return "FAILED_ALLOCATE_MEM_ERR";
 }
 
 
-inline
+ 
 const std::string ErrorCatcher::noBufferError() {
 	return "NO_BUFFER_ERR";
 }
 
 
-inline
+ 
 const std::string ErrorCatcher::readSocketError() {
 	return "READ_SOCKET_ERR";
 }
 
 
-inline
+ 
 const std::string ErrorCatcher::writeSocketError() {
 	return "WRITE_SOCKET_ERR";
 }
 
 
-inline
+ 
 const std::string ErrorCatcher::receiveExitSignalError() {
 	return "RECEIVE_EXIT_SIGNAL_ERR";
 }
