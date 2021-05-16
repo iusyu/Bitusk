726fcddf508b68ee61ca1fbe23b01f264204173f
IYNUS <iuamyy@163.com>
2021-05-16 12:15:16 +0800

    rewrite parsing

--- include/BtParse.h
+++ include/BtParse.h
@@ -6,82 +6,145 @@
 * @birth    : 2021-05-01 23:48
 * @author   : sunyi
 * @version  : 0.1.0
-* @revisions: NA
+* @revisions: 2021-05-15
 ==============================================
 */
 
-// * Support this interface Logger << date <<info<< ...;
-
 
 #pragma once
+
 #include <string>
 #include <iostream>
 #include <sstream>
 #include <fstream>
 #include <memory>
 #include <utility>
+#include <vector>
+#include <map>
+
+class ParseBittorrentFile;
+
+/* Bittorrent File keyword struct */
+struct BittorrentData {
+	BittorrentData();
+
+	bool empty() const ;
+
+	/* keyword 'info' has value of diction 
+	 * 
+	 * to get keyvalue by 'info["name"]'
+	 *
+	 * common keyword contained
+	 * @piece_length : length of every piece
+	 * @pieces 	 : a string of hash value
+	 * @private 	 : ......
+	 *
+	 * single file mode
+	 * @name
+	 * @length
+	 * @md5sum
+	 *
+	 * multifile mode
+	 * @name 	 : store all shared files' directory name
+	 * @files	 : have a directory value specifing in 'std::map<std::string, std::string> files '
+	 * */
+	std::map<std::string, std::string> info;
 
 
+	/* keyword 'info' has keyword 'files' in multifile mode
+	 * @length 	 : shared file length, Unit is Byte
+	 * @md5sum
+	 * @path	 : store the path and name of shared files
+	 */
+	std::map<std::string, std::string> files;
 
 
-/* 种子文件解析中 */
-class AbsParse {
+	/* Tracker URL */
+	std::string announce;
+
+
+	/* Tracker URL list */
+	std::vector<std::string> announceList;
+};
+
+
+/* using abstract factory method */
+class AbsParseMethod {
 public:
-	AbsParse() = default;
-	AbsParse(const AbsParse& ) = delete;
-	AbsParse(const AbsParse&&);
-	AbsParse(const std::string& bf):ifbtstream(bf) {}
+	virtual AbsParseMethod* getParseMethod(const std::string&);
+	virtual ~AbsParseMethod();
+private:
+	BittorrentData* str;
+};
+
+
+/* inherit from AbsParseMethod */
+class PAnnounce: public AbsParseMethod {
+public:
+	virtual AbsParseMethod* getParseMethod(const std::string&) override;
 	
 
-	/* Application Programming Interface  */
-
-	/*
-	 * @brief: find position of value which key specific
-	 * @param: key of key-value pair
-	 * @ret: begining position of value 
-	 * @birth: 2021-05-11
-	 */
-	virtual std::string::iterator findKeyPos(const std::string&);
-
-	/*
-	 * @brief: find value with key
-	 * @param: key which to find [const std::string& ]
-	 * @ret: value of key
-	 * @birth: 2021-05-11
-	 */
-	virtual const std::string findKeyValue(const std::string&);
-
 	
+};
+
+
+/* different method of parse keyvalue */
+
+class AbsParseInfo{
+public:
+	typedef std::map<std::string,std::string> infoDic;
+
+	AbsParseInfo() = delete;
+	AbsParseInfo(const AbsParseInfo&) = delete;
+
+	AbsParseInfo(ParseBittorrentFile* p):pbf(p) {}
+	
+	virtual infoDic parseInfo() = 0;
+	virtual ~AbsParseInfo();
+	
+private:
+	ParseBittorrentFile* pbf;
+};
+
+
+class ParseInfo:public AbsParseInfo {
+public:
+	ParseInfo(ParseBittorrentFile* p):AbsParseInfo(p) {}
+	
+
+	virtual infoDic parseInfo() override ;
+};
+
+
+/* the basic tools for parse String */
+class ParseBittorrentFile {
+public:
+	ParseBittorrentFile() = delete;
+	ParseBittorrentFile(const ParseBittorrentFile&) = delete;
+	ParseBittorrentFile(const std::string& );
+	virtual const std::string& get();
+	
+	virtual const std::string& getString(const std::string& );
+
+private:
+	virtual const std::string& getInfoOf(const std::string&);
+	virtual const std::string& getFilesOf(const std::string&);
+	virtual const std::vector<std::string>& getListOf(const std::string&);
+	void parseIt();
+	bool checkParse();
+
+
+
 
 
 private:
-	std::ifstream ifbtstream;
+	BittorrentData data;
+	std::string bittorrentFileString;
+	std::map<std::string,std::shared_ptr<AbsParseMethod>> parseMethod;
 };
 
 
 
-class Parse:public AbsParse{
-public:
-	
-	Parse();
 
-	/*
-	 * @brief: find value with key
-	 * @param: key which to find [const std::string& ]
-	 * @ret: value of key
-	 * @birth: 2021-05-11
-	 */
-	virtual const std::string findKeyValue(const std::string&) override;
-
-	/*
-	 * @brief: find value with key
-	 * @param: key which to find [const std::string& ]
-	 * @ret: value of key
-	 * @birth: 2021-05-11
-	 */
-	virtual std::string::iterator findKeyPos(const std::string&) override;
-
-
-};
 
 
--- src/BtParse.cpp
+++ src/BtParse.cpp
@@ -1,38 +1,69 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iterator>
 
 #include "BtParse.h"
 
 
 
-AbsParse::AbsParse(const AbsParse&& ap):ifbtstream(std::move(ap.ifbtstream)){
+/* struct BittorrentData IMP */
+BittorrentData::BittorrentData() {
+	info["piece_length"] = "";
+	info["pieces"] 	     = "";
+	info["private"]      = "";
+
+	/* single file */
+	info["name"]	     = "";
+	info["length"]	     = "";
+	info["md5sum"] 	     = "";
+
+	/* multifile mode */
+	info["name"]         = "";
+
+	files["length"]      = "";
+	files["path"]        = "";
+	files["md5sum"]	     = "";
 }
 
 
-std::string::iterator AbsParse::findKeyPos(const std::string& key) {
+bool BittorrentData::empty() const {
+	return info.empty();
+}
+
+
+/* class AbsParseMethod IMP */
+AbsParseMethod* AbsParseMethod::getParseMethod(const std::string& key) {
+	return this;
+}
+
+
+
+/* class ParseBittorrentFile IMP */
+ParseBittorrentFile::ParseBittorrentFile(const std::string& fileName) {
+	std::ifstream f(fileName);
+	if(f) {
+		std::string tmpStr{std::istreambuf_iterator<char>(f), std::istreambuf_iterator<char>()};
+		bittorrentFileString = tmpStr;
+	}
+}
+
+
+const std::string& ParseBittorrentFile::getString(const std::string& key) {
 	
 }
 
 
-const std::string AbsParse::findKeyValue(const std::string& key) {
+const std::string& ParseBittorrentFile::getInfoOf(const std::string& key) {
 	
 }
 
 
-Parse::Parse() {
-
-}
-
-
-const std::string::iterator Parse::findKeyPos(const std::string& key) {
+void ParseBittorrentFile::parseIt() {
 	
 }
 
 
-const std::string Parse::findKeyValue(const std::string& key) {
-
+bool ParseBittorrentFile::checkParse() {
+	return data.empty();
 }
-
-
-
