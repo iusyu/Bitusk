94fd3eb82b815534d8d4e9caf4406bbb48f9d417
iusk <iuamyy@163.com>
2021-05-22 22:17:02 +0800

    bit field interface

--- include/Btbitfield.h
+++ include/Btbitfield.h
@@ -12,5 +12,33 @@
 
 #pragma once
 
+#include <iostream>
+#include <memory>
 
+class Bitmap{
+public:
+	Bitmap();
+	Bitmap(std::size_t b):validBits(b),bitfieldLength(validBits/8 +1), bits(new char[bitfieldLength]) {}
+	Bitmap(const Bitmap&) = delete;
+	Bitmap& setBit(size_t indx);
+	Bitmap& setBitOn(size_t indx);
+	Bitmap& setBitOff(size_t indx);
+	Bitmap& setAllZero();
+	Bitmap& setAllOne();
 
+	bool getBitStatus(size_t indx);
+	std::size_t countOffBit() const;
+	std::size_t countOnBit() const;
+	Bitmap& display() const;
+	bool operator==(const Bitmap&);
+
+private:
+	// Used bits;
+	std::size_t validBits;
+
+	// Allocated bits;
+	std::size_t bitfieldLength;
+
+	std::unique_ptr<char[]> bits;
+
+};
