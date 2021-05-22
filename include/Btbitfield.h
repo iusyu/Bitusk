15f0e44b8fb27a4eb261977fe10e25e56186e634
iusk <iuamyy@163.com>
2021-05-22 16:05:32 +0800

    bitfield class to manager downloader

---
+++ include/Btbitfield.h
@@ -0,0 +1,16 @@
+/*
+=============================================
+* @brief    : Bt bit field manager 
+* @copyright: Copyright 2021 iusyu
+* @license  : Apache
+* @birth    : 2021-05-22 23:48
+* @author   : sunyi
+* @version  : 0.1.0
+* @revisions: 
+==============================================
+*/
+
+#pragma once
+
+
+
