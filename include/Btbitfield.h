cf476becd970a56e42faf4284fe357574ac871e0
iusk <iuamyy@163.com>
2021-05-24 22:47:29 +0800

    work on imp

--- include/Btbitfield.h
+++ include/Btbitfield.h
@@ -15,22 +15,49 @@
 #include <iostream>
 #include <memory>
 
+static const unsigned char bitOn[8]   = {0b10000000,
+					0b01000000,
+					0b00100000,
+					0b00010000,
+					0b00001000,
+					0b00000100,
+					0b00000010,
+					0b00000001};
+static const unsigned char bitMask[8] ={0b01111111,
+					0b10111111,
+					0b11011111,
+					0b11101111,
+					0b11110111,
+					0b11111011,
+					0b11111101,
+					0b11111110};
+
+static const unsigned char bitAllOn  = 0b11111111;
+static const unsigned char bitAllOff = 0b00000000;
+
 class Bitmap{
 public:
 	Bitmap();
 	Bitmap(std::size_t);
+	Bitmap(std::size_t, std::size_t, std::size_t);
 	Bitmap(const Bitmap&) = delete;
-	Bitmap& setBit(size_t indx);
+	Bitmap& reverseBit(size_t indx);
+	Bitmap& reverseAllBit();
 	Bitmap& setBitOn(size_t indx);
 	Bitmap& setBitOff(size_t indx);
 	Bitmap& setAllZero();
 	Bitmap& setAllOne();
 
-	bool getBitStatus(size_t indx);
+	bool getBitStatus(size_t indx) ;
 	std::size_t countZeroBit() const;
 	std::size_t countOneBit() const;
 	Bitmap& display() const;
 	bool operator==(const Bitmap&);
+
+	~Bitmap();
+private:
+
+	Bitmap& getPos(size_t ,size_t *, size_t*);
 
 private:
 	// Used bits;
@@ -40,9 +67,18 @@ private:
 	std::size_t bitfieldLength;
 
 	// If just need 1 Byte ?
-	std::unique_ptr<char[]> bits;
+	std::unique_ptr<unsigned char[]> bits;
+	unsigned char *pbits;
 
 	std::size_t zeroBitsNumber;
 	std::size_t oneBitsNumber;
 
+	bool rwStatus;
+
 };
+
+
+/* out stream */
+
+std::ostream& operator<<(std::ostream& , const Bitmap&);
+std::istream& operator>>(std::istream&, Bitmap&);
--- src/Btbitfield.cpp
+++ src/Btbitfield.cpp
@@ -16,7 +16,115 @@
 Bitmap::Bitmap():validBits(0),bitfieldLength(0),zeroBitsNumber(0),oneBitsNumber(0) {
 }
 
-Bitmap::Bitmap(std::size_t b):validBits(b),bitfieldLength(validBits/8 +1), bits(new char[bitfieldLength]) {
+Bitmap::Bitmap(std::size_t b):validBits(b),bitfieldLength(validBits/8 +1), bits(new unsigned char[bitfieldLength]) {
 	setAllZero();
+	zeroBitsNumber = validBits;
+	oneBitsNumber = validBits - b;
 }
+
+
+Bitmap::Bitmap(std::size_t b, std::size_t begin, std::size_t end):Bitmap(b) {
+	// TODO a alittle bit hard
+}
+
+
+Bitmap& Bitmap::reverseBit(size_t indx) {
+}
+
+
+Bitmap& Bitmap::reverseAllBit() {
+}
+
+
+Bitmap& Bitmap::setBitOn(size_t indx) {
+	if(indx > validBits)
+		return *this;
+	size_t charNum = 0, wiseIndx = 0;
+	getPos(indx, &charNum, &wiseIndx);
+	pbits[charNum] |= bitOn[ wiseIndx ];
+	return *this;
+}
+
+
+Bitmap& Bitmap::setBitOff(size_t indx) {
+	if( indx > validBits)
+		return *this;
+	size_t charNum = 0, wiseIndx = 0;
+	getPos(indx, &charNum, &wiseIndx);
+	pbits[charNum] ^= bitOn[wiseIndx];
+	return *this;
+}
+
+
+Bitmap& Bitmap::setAllZero() {
+
+	for( size_t i = 0; i < bitfieldLength / 8; i++ ) {
+		pbits[i] = bitAllOff;
+	}
+	return *this;
+}
+
+
+Bitmap& Bitmap::setAllOne() {
+	for( size_t i = 0; i < bitfieldLength / 8; i++ ) {
+		pbits[i] = bitAllOn;
+	}
+	return *this;
+}
+
+
+bool Bitmap::getBitStatus(size_t indx){
+	if(indx > validBits){
+		rwStatus = false;
+		return false;
+	}
+
+	size_t charNum = 0, wiseIndx = 0;
+	getPos(indx, &charNum, &wiseIndx);
+	unsigned char tmp = pbits[charNum];
+	return  bitOn[wiseIndx];
+}
+
+
+size_t Bitmap::countOneBit() const {
+}
+
+
+size_t Bitmap::countZeroBit() const {
+}
+
+
+Bitmap& Bitmap::display() const {
+}
+
+
+bool Bitmap::operator==( const Bitmap& bitmap) {
+	
+}
+
+
+Bitmap::~Bitmap() {
+	delete[] pbits;
+}
+
+
+Bitmap& Bitmap::getPos(size_t idx, size_t* charBitPos, size_t* charIndx) {
+	if( charBitPos == nullptr || charIndx == nullptr)
+		return *this;
+	*charBitPos = idx / 8 +1;
+	*charIndx = idx % 8;
+	return *this;
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Bitmap& bitmap) {
+
+	return os;
+}
+
+
+std::istream& operator>>(std::istream& is, Bitmap& bitmap) {
+	return is;
+}
+
 
