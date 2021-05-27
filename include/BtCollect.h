8b2a649363aedba593a394c47e730daa10359108
iusk <iuamyy@163.com>
2021-05-27 20:45:27 +0800

    header Collection

---
+++ include/BtCollect.h
@@ -0,0 +1,15 @@
+/*
+ * * * * * * * * * * * * * * * *
+ *
+ * Collection of bt downloader header
+ *
+ *
+ * * * * * * * * * * * * * * * *
+ */
+
+#pragma once 
+
+#include "BtError.h"
+#include "BtLogger.h"
+#include "Btbitfield.h"
+
