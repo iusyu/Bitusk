ff3e510a55069164d3d375308f28b3dba92c30de
iusk <iuamyy@163.com>
2021-06-20 17:28:39 +0800

    Message Header

---
+++ include/Message.h
@@ -0,0 +1,40 @@
+/*
+=============================================
+* @brief    : Bt downloader logger 
+* @copyright: Copyright 2021 iusyu
+* @license  : Apache
+* @birth    : 2021-05-01 23:48
+* @author   : sunyi
+* @version  : 0.1.0
+* @revisions: 2021-05-22
+==============================================
+*/
+
+
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <memory>
+#include <utility>
+#include <vector>
+#include <map>
+#include <bitset>
+#include <list>
+
+#include "Peer.h"
+
+class MessageGenerator{
+public:
+	typedef std::string msg;
+	
+};
+
+
+class MessageGeneratorImp: public MessageGenerator{
+public:
+	
+};
+
